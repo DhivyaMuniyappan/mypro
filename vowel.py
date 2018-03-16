cc8775ee290ad545bb009e4f5f0992ce2949a420
DhivyaMuniyappan <36299454+DhivyaMuniyappan@users.noreply.github.com>
2018-03-16 17:33:24 +0530

    vowel

---
+++ vowel.py
@@ -0,0 +1,13 @@
+vowel=['a','e','i','o','u','A','E','I','O','U']
+str=list(input("Enter the string"))
+def vw(str):
+    for i in vowel:
+        if i in str:
+            return "yes"
+    return "no"
+print(vw(str))
+
+
+
+
+
