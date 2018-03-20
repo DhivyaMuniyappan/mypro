96328ca208cb52a5118241654580217b7924f42c
DhivyaMuniyappan <36299454+DhivyaMuniyappan@users.noreply.github.com>
2018-03-20 17:33:26 +0530

    SI

--- SI.py
+++ SI.py
@@ -1,5 +1,7 @@
+import math
 P=int(input("Enter principal amount:"))
 T=float(input("For Time period:"))
 R=float(input("Rate:"))
 SI=(P*T*R)/100
 print("Simple Interest is:",SI)
+print(math.floor(SI))
